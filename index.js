657e6394a3b1e5cb714367a468add2e4a6f86693
Edcel Jeremy Macawili <edcel.macawili@dottystylecreative.com>
2026-01-30 10:06:31 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configure Pug as view engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic GET / route
+app.get("/", (req, res) => {
+	res.send("Server running");
+});
+
+// 404 handler
+app.use((req, res, next) => {
+	res.status(404).send("404 - Page Not Found");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("500 - Something went wrong!");
+});
+
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
